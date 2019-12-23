8402ba532e54e83e282198ab7289a20aefc7aa8d
Eric Jansen <ericjansen@live.nl>
2019-12-23 10:12:59 +0100

    initial commmit

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,23 @@
-import { sum } from './index';
+import Encryption from './Encryption';
+const encryption = new Encryption();
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+const key = encryption.generateRandomKey(32);
+
+const testPassword = 'Test123!';
+
+test('Hash string without errors', () => {
+    expect(() => encryption.hashString(testPassword)).not.toThrow();
+});
+
+test('Generate random key without errors', () => {
+    expect(() => encryption.generateRandomKey(32)).not.toThrow();
+});
+
+test('Encrypt string and return object', () => {
+    expect(() => encryption.encryptString(testPassword, key)).not.toThrow();
+});
+
+test('Decrypt string and return object', () => {
+    const encrypted = encryption.encryptString(testPassword, key);
+    expect(() => encryption.decryptString(encrypted.encryptedString, key, encrypted.iv)).not.toThrow();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+export { default } from './Encryption';
+
 export class C {
-  private x = 10;
-  public getX = (): number => this.x;
-  public setX = (newVal: number): void => {
-    this.x = newVal;
-  };
+    private x = 10;
+    public getX = (): number => this.x;
+    public setX = (newVal: number): void => {
+        this.x = newVal;
+    };
 }
 
 export const x = new C();
 export const y = { ...{ some: 'value' } };
 
 export function sum(a: number, b: number): number {
-  return a + b;
+    return a + b;
 }
